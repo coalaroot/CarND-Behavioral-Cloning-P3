a674c5af1926e396ff54ebad700c3d7eece20d86
coalaroot <jakubkrolak94@gmail.com>
2017-06-01 15:22:04 +0200

    cleaning

--- model.py
+++ model.py
@@ -14,9 +14,11 @@ with open('./data/driving_log.csv') as csvfile:
         samples.append(line)
 
 train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+# Correction for left & right camera of the vehicle (see in generator())
 correction = 0.1
 
 
+# Using Keras generator for optimization
 def generator(samples, batch_size=32):
     num_samples = len(samples)
     while 1:
@@ -27,13 +29,13 @@ def generator(samples, batch_size=32):
             images = []
             angles = []
             for batch_sample in batch_samples:
-                for i in range(3):
+                for i in range(3):  # Using all 3 cameras
                     name = './data/IMG/' + batch_sample[i].split('/')[-1]
                     center_image = cv2.imread(name)
                     center_angle = float(batch_sample[3])
-                    if i == 1:  # left camera
+                    if i == 1:  # left camera correction
                         center_angle += correction
-                    elif i == 2:  # right camera
+                    elif i == 2:  # right camera correction
                         center_angle -= correction
 
                     images.append(center_image)
@@ -49,11 +51,13 @@ def generator(samples, batch_size=32):
 train_generator = generator(train_samples, batch_size=32)
 validation_generator = generator(validation_samples, batch_size=32)
 
+# Dimensions of the images
 ch, row, col = 3, 160, 320
 
+# Using NVIDIA CNN architecture (https://devblogs.nvidia.com/parallelforall/deep-learning-self-driving-cars/)
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(row, col, ch), output_shape=(row, col, ch)))
-model.add(Cropping2D(cropping=((60, 25), (0, 0))))
+model.add(Cropping2D(cropping=((60, 25), (0, 0))))  # Cropping the non-road pixels
 model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation="relu"))
 model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation="relu"))
 model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation="relu"))
@@ -65,6 +69,8 @@ model.add(Dense(50))
 model.add(Dense(10))
 model.add(Dense(1))
 
+# Using mse because it gives the best results
+# Found out that more that 2 epochs is overfitting
 model.compile(loss='mse', optimizer='adam')
 model.fit_generator(train_generator,
                     samples_per_epoch=len(train_samples) * 6,  # because of data augmentation
